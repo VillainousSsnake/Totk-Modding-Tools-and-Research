a65ab580704900d9e3955487eaeee139bc09ba00
VillainousSsnake <144048072+VillainousSsnake@users.noreply.github.com>
2023-12-12 18:45:18 +0000

    Modified zsdic_stucture_seperator_tool.py

--- zstandard_dictionary/zsdic_structure_seperator_tool.py
+++ zstandard_dictionary/zsdic_structure_seperator_tool.py
@@ -1,9 +1,12 @@
 import struct
+import os
+
+import struct
 
 def extract_dictionary_parts(dictionary_file_path):
     with open(dictionary_file_path, 'rb') as file:
         # Read and parse the header
-        header_data = file.read(16)  # Adjust the size based on the actual header structure
+        header_data = file.read(8)  # Adjust the size based on the actual header structure
         with open('header.txt', 'wb') as header_file:
             header_file.write(header_data)
 
@@ -13,8 +16,27 @@ def extract_dictionary_parts(dictionary_file_path):
         with open('literals.txt', 'wb') as literals_file:
             literals_file.write(literals_data)
 
+        # Read and parse the symbol frequencies
+        frequencies_size = struct.unpack('<Q', file.read(8))[0]
+        frequencies_data = file.read(frequencies_size)
+        with open('frequencies.txt', 'wb') as frequencies_file:
+            frequencies_file.write(frequencies_data)
+
+        # Read and parse the variable-length code assignments
+        codes_size = struct.unpack('<Q', file.read(8))[0]
+        codes_data = file.read(codes_size)
+        with open('codes.txt', 'wb') as codes_file:
+            codes_file.write(codes_data)
+
         # Read and parse other sections as needed...
 
 if __name__ == "__main__":
     dictionary_path = 'path/to/your/dictionary.zdict'
     extract_dictionary_parts(dictionary_path)
+
+if __name__ == "__main__":
+    os.makedirs(os.path.join("zsdic", "zs"))
+    os.makedirs(os.path.join("zsdic", "pack"))
+    os.makedirs(os.path.join("zsdic", "byml_bcett"))
+    for dictionary_path in ["zs.zsdic", "pack.zsdic", "bcett.byml.zsdic"]:
+        extract_dictionary_parts(dictionary_path)
