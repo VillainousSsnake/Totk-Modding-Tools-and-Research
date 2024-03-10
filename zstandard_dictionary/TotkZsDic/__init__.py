653a15ebdafd1e0974fa3aefe1f0c82ba3be3c75
VillainousSsnake <cjdiamond9@gmail.com>
2024-03-10 12:29:21 -0400

    Added comments

--- zstandard_dictionary/TotkZsDic/__init__.py
+++ zstandard_dictionary/TotkZsDic/__init__.py
@@ -10,6 +10,7 @@ class ZsDic:
         :return: bytes or binary data
         """
 
+        # Creating the zip controller
         zip_controller = zipfile.ZipFile(
             os.path.join(
                 os.path.dirname(
@@ -20,20 +21,22 @@ class ZsDic:
             ), mode='r'
         )
 
+        # Match statement for the dict_type
         match dict_type:
 
-            case 'zs':
+            case 'zs':  # If dict_type is 'zs'
                 return zip_controller.read(name='zs.zsdic')
 
-            case 'pack':
+            case 'pack':    # If dict_type is 'pack'
                 return zip_controller.read(name='pack.zsdic')
 
-            case 'bcett':
+            case 'bcett':   # If dict_type is 'bcett'
                 return zip_controller.read(name='bcett.byml.zsdic')
 
-            case 'byml':
+            case 'byml':    # If dict_type is 'byml'
                 return zip_controller.read(name='bcett.byml.zsdic')
 
+        # If dict_type wasn't a valid value, raise error
         raise ValueError(
             "dict_type param may either be 'zs', 'pack', 'bcett', or 'byml'. It can't be " + dict_type
         )
