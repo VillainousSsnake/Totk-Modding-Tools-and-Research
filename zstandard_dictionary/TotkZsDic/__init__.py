5ac5293bd5a127a42d4c27f357aae154b3021df2
VillainousSsnake <cjdiamond9@gmail.com>
2024-03-10 12:27:20 -0400

    Removed init function and made get_dict static

--- zstandard_dictionary/TotkZsDic/__init__.py
+++ zstandard_dictionary/TotkZsDic/__init__.py
@@ -3,9 +3,14 @@ import zipfile
 
 
 class ZsDic:
-    def __init__(self):
+    @staticmethod
+    def get_dict(dict_type: str) -> bytes:
+        """
+        :param dict_type: type of dictionary to return. Can be 'zs', 'pack', 'bcett', or 'byml'
+        :return: bytes or binary data
+        """
 
-        self.zip_controller = zipfile.ZipFile(
+        zip_controller = zipfile.ZipFile(
             os.path.join(
                 os.path.dirname(
                     os.path.abspath(
@@ -15,25 +20,19 @@ class ZsDic:
             ), mode='r'
         )
 
-    def get_dict(self, dict_type: str):
-        """
-        :param dict_type: type of dictionary to return. Can be 'zs', 'pack', 'bcett', or 'byml'
-        :return: bytes or binary data
-        """
-
         match dict_type:
 
             case 'zs':
-                return self.zip_controller.read(name='zs.zsdic')
+                return zip_controller.read(name='zs.zsdic')
 
             case 'pack':
-                return self.zip_controller.read(name='pack.zsdic')
+                return zip_controller.read(name='pack.zsdic')
 
             case 'bcett':
-                return self.zip_controller.read(name='bcett.byml.zsdic')
+                return zip_controller.read(name='bcett.byml.zsdic')
 
             case 'byml':
-                return self.zip_controller.read(name='bcett.byml.zsdic')
+                return zip_controller.read(name='bcett.byml.zsdic')
 
         raise ValueError(
             "dict_type param may either be 'zs', 'pack', 'bcett', or 'byml'. It can't be " + dict_type
